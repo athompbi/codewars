8cda95763dd832198dd6c4d71044291666228374
cloggerish <104546948+cloggerish@users.noreply.github.com>
2022-08-04 23:53:30 -0600

    Added well of ideas kata

--- codewars_august.py
+++ codewars_august.py
@@ -31,7 +31,44 @@ def dig_pow(n, p):
 ######################################################################
 
 
+### 2
+### Well of ideas - Easy Version
+
+def well(x):
+    """Determines how many 'good' ideas are in a given
+    list and returns text based on how many 'good' ideas
+    are in the list
+    
+    Return:
+        Text based on number of 'good' ideas in list"""
+
+    good_count = 0
+
+    for item in x:
+        if item == 'good':
+            good_count += 1
+        
+    if good_count == 1 or good_count == 2:
+        return 'Publish!'
+    elif good_count > 2:
+        return 'I smell a series!'
+    else:
+        return 'Fail!'
+
+
+######################################################################
 
 
 
+import math
 
+
+def decimal_binary_converter(decimal):
+    
+    bits = math.ceil(math.log(decimal, 2))
+
+    return bits
+
+print(decimal_binary_converter(8))
+
+print(math.log(8,2))
